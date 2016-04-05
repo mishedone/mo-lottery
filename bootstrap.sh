38fb882802c6455b6a01ff2492b5ee38cf34b861
Mihail Milev <m.milev@vuk.bg>
2016-04-05 09:37:14 +0300

    Fix php integration with apache.

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,10 @@ fi
 apt-get install -y python-software-properties
 apt-get install -y php7.0 php7.0-fpm
 apt-get install -y libapache2-mod-php7.0
+sed -i "s|display_errors = Off|display_errors = On|" /etc/php/7.0/apache2/php.ini
+sed -i "s|APACHE_RUN_USER=www-data|APACHE_RUN_USER=vagrant|" /etc/apache2/envvars
+sed -i "s|APACHE_RUN_GROUP=www-data|APACHE_RUN_GROUP=vagrant|" /etc/apache2/envvars
+service apache2 restart
 
 # clean up
 apt-get --purge -y autoremove
