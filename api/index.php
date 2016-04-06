08a4469d71565e89fc28c50e156db0a95d4c1ac2
Mihail Milev <m.milev@vuk.bg>
2016-04-06 17:28:28 +0300

    Load data for different years and save it in database.

--- api/index.php
+++ api/index.php
@@ -1,15 +1,38 @@
 <?php
 
-$url = 'http://www.toto.bg/files/tiraji/649_58.txt';
-
-$urlContent = file_get_contents($url);
-$editions = array();
-foreach (explode("\n", $urlContent) as $rawEdition) {
-    $towing = explode(',', $rawEdition);
-    unset($towing[0]);
-
-    $editions[] = array_values($towing);
+$db = array();
+if (file_exists('data/editions.json')) {
+    $db = json_decode(file_get_contents('data/editions.json'), true);
 }
 
-header('Content-Type: application/json');
-echo json_encode($editions);
+$update = false;
+for ($year = 1958; $year <= 1965; $year++) {
+    if (!isset($db[$year])) {
+        echo sprintf('Reloading db for %d year...' . "\r\n", $year);
+
+        $urlYear = ($year < 2005) ? substr($year, 2) : $year;
+        $url = sprintf('http://www.toto.bg/files/tiraji/649_%d.txt', $urlYear);
+        $urlContent = file_get_contents($url);
+
+        $editions = array();
+        foreach (explode("\n", $urlContent) as $rawEdition) {
+            $towing = explode(',', $rawEdition);
+            unset($towing[0]);
+
+            if (count($towing)) {
+                $editions[] = array_values($towing);
+            }
+        }
+
+        $db[$year] = $editions;
+
+        $update = true;
+    }
+}
+
+if ($update) {
+    file_put_contents('data/editions.json', json_encode($db));
+} else {
+    header('Content-Type: application/json');
+    echo json_encode($db);
+}
