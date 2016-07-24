0b1071a3359354659c80b2d7fff3ab9123b0ac0e
mishedone <michamilev@gmail.com>
2016-07-24 19:48:24 +0300

    Load providers through the api object.

--- src/app.js
+++ src/app.js
@@ -2,37 +2,16 @@ function App() {
     this.api = new Api();
     this.router = new Router();
     
-    this.render = function () {
+    this.renderNavigation = function (providers) {
         var navigation = new NavigationView({
             el: '#navigation-slot',
-            providers: [
-                {
-                    games: [
-                        {
-                            id: 'rocket',
-                            name: 'Rocket'
-                        }
-                    ],
-                    id: 'acme',
-                    name: 'ACME'
-                },
-                {
-                    games: [
-                        {
-                            id: '642',
-                            name: '6/42'
-                        },
-                        {
-                            id: '649',
-                            name: '6/49'
-                        }
-                    ],
-                    id: 'bst',
-                    name: 'Bulgarian TOTO'
-                }
-            ]
+            providers: providers
         });
         navigation.render();
+    };
+    
+    this.render = function () {
+        this.api.getProviders(this.renderNavigation);
         
         // start router
         Backbone.history.start();
