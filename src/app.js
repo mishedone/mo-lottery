0659240fbaebe6b97a346978b26c3afaf7960023
mishedone <michamilev@gmail.com>
2016-07-24 19:57:32 +0300

    Write a todo list with the next steps.

--- src/app.js
+++ src/app.js
@@ -20,3 +20,12 @@ function App() {
 
 var app = new App();
 app.render();
+
+/**
+ * TODOS:
+ * 1. create provider-game model that has providerId, providerName, gameId, gameName properties
+ * 2. choose provider-game for initial load
+ * 3. create provider-game storage and save current state in there
+ * 4. show last saved current provider-game when loading the application
+ * 5. trigger on game changed event and save new state
+ */
