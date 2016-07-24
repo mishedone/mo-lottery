d575adaca6ea05bae25a8c951f4ef3fec5cee16f
mishedone <michamilev@gmail.com>
2016-07-24 19:44:03 +0300

    Render drop down with the passed in providers to the navigation view.

--- src/views/navigation/navigation.js
+++ src/views/navigation/navigation.js
@@ -1,8 +1,15 @@
 var NavigationView = Backbone.View.extend({
     template: _.template($('#navigation').html()),
+    providers: [],
 
+    initialize: function (options) {
+        this.providers = options.providers;
+    },
+    
     render: function () {
-        this.$el.html(this.template());
+        this.$el.html(this.template({
+            providers: this.providers
+        }));
 
         return this;
     }
