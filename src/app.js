7251fd923620253434c48064d3c7e1261aef2fa3
m.milev <m.milev@vuk.bg>
2016-07-25 13:27:58 +0300

    Handle change game events in navigation view and switch current game.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ function App() {
     this.currentGame = {};
     this.currentGameStorage = new CurrentGameStorage();
     this.router = new Router();
+}
 
-    this.start = function () {
+App.prototype = _.extend({}, Backbone.Events, {
+    constructor: App,
+
+    start: function () {
         var app = this;
 
         this.games = new GameCollection();
@@ -14,32 +18,40 @@ function App() {
                 app.render();
             }
         });
-    };
+    },
 
-    this.loadCurrentGame = function () {
+    loadCurrentGame: function () {
         if (!this.currentGameStorage.has()) {
             this.currentGameStorage.set(this.games.first());
         }
 
         this.currentGame = this.currentGameStorage.get();
-    };
-    
-    this.render = function () {
+    },
+
+    render: function () {
         this.renderNavigation();
-        
+
         // start router
         Backbone.history.start();
-    };
+    },
 
-    this.renderNavigation = function () {
+    renderNavigation: function () {
         var navigation = new NavigationView({
             el: '#navigation-slot',
             games: this.games,
             currentGame: this.currentGame
         });
+        this.listenTo(navigation, 'game:change', this.changeGame);
         navigation.render();
-    };
-}
+    },
+
+    changeGame: function (game) {
+        this.currentGameStorage.set(game);
+
+        // refresh current page
+        Backbone.history.loadUrl(Backbone.history.fragment);
+    }
+});
 
 var app = new App();
 app.start();
--- src/views/navigation/navigation.js
+++ src/views/navigation/navigation.js
@@ -3,6 +3,10 @@ var NavigationView = Backbone.View.extend({
     games: [],
     currentGame: {},
 
+    events: {
+        'click .game': 'changeGame'
+    },
+
     initialize: function (options) {
         this.games = options.games;
         this.currentGame = options.currentGame;
@@ -15,5 +19,22 @@ var NavigationView = Backbone.View.extend({
         }));
 
         return this;
+    },
+
+    changeGame: function (event) {
+        var game, gameId;
+        event.preventDefault();
+
+        // identify selected game
+        gameId = this.$(event.target).data('game-id');
+        game = this.games.findWhere({
+            id: gameId
+        });
+
+        // update view
+        this.$('.current-game').html(game.get('name'));
+
+        // trigger change event
+        this.trigger('game:change', game);
     }
 });
