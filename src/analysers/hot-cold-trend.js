661de79137239d5c56f01b6796dfda0294cca309
m.milev <m.milev@vuk.bg>
2016-07-27 15:02:52 +0300

    Collect total hits in hot-cold trend analysis.

--- src/analysers/hot-cold-trend.js
+++ src/analysers/hot-cold-trend.js
@@ -1,9 +1,46 @@
-function HotColdTrendAnalyser() {}
+function HotColdTrendAnalyser() {
+    this.reset({});
+}
 
 HotColdTrendAnalyser.prototype = {
     constructor: HotColdTrendAnalyser,
 
-    analyse: function () {
-        console.log('analysing...');
+    reset: function (game) {
+        this.game = game;
+        this.currentYear = null;
+        this.hits = {};
+    },
+
+    analyse: function (game) {
+        var analyser = this;
+
+        analyser.reset(game);
+        analyser.game.loadYears(function () {
+            analyser.analyseYears();
+        });
+    },
+
+    analyseYears: function () {
+        var analyser = this;
+
+        analyser.game.get('years').forEach(function (year) {
+            analyser.currentYear = year.get('year');
+            analyser.game.loadDraws(year.get('year'), function () {
+                analyser.analyseDraws();
+            });
+        });
+    },
+
+    analyseDraws: function () {
+        var analyser = this;
+
+        analyser.game.getDraws(this.currentYear).forEach(function (draw) {
+            _.each(draw.get('draw'), function (number) {
+                if (!analyser.hits.hasOwnProperty(number)) {
+                    analyser.hits[number] = 0;
+                }
+                analyser.hits[number]++;
+            });
+        });
     }
 };
