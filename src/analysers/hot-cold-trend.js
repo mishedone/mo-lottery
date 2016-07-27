043302bf40fdefaa8e0e8754080787b37975a73a
m.milev <m.milev@vuk.bg>
2016-07-27 13:08:37 +0300

    Create skeleton for the hot-cold trend analyser class.

---
+++ src/analysers/hot-cold-trend.js
@@ -0,0 +1,9 @@
+function HotColdTrendAnalyser() {}
+
+HotColdTrendAnalyser.prototype = {
+    constructor: HotColdTrendAnalyser,
+
+    analyse: function () {
+        console.log('analysing...');
+    }
+};
