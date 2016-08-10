491f1d2a81d6f9b0c4140d2a039f5f13a3a814e5
m.milev <m.milev@vuk.bg>
2016-08-10 09:59:48 +0300

    Calculate number risings in hot-cold trend suggestions.

--- src/analysers/hot-cold-trend/suggestions.js
+++ src/analysers/hot-cold-trend/suggestions.js
@@ -7,7 +7,8 @@ HotColdTrendSuggestions.prototype = {
 
     reset: function (game) {
         this.game = game;
-        this.result = {};
+        this.map = {};
+        this.result = [];
     },
 
     get: function (game, success) {
@@ -21,12 +22,56 @@ HotColdTrendSuggestions.prototype = {
     },
 
     calculate: function (analysis) {
-        var suggestions = this;
+        var suggestions = this, averageHit;
 
-        _.each(analysis, function (period, periodNumber) {
+        averageHit = analysis[0].averageHit;
+
+        // initialize result
+        _.each(analysis[0].stats, function (data) {
+            if (data.hits > averageHit) {
+                suggestions.result.push({
+                    number: data.number,
+                    previousHits: data.hits,
+                    risings: 1,
+                    stillRising: true
+                });
+                suggestions.map[data.number] = suggestions.result.length - 1;
+            }
+        });
+
+        // collect risings
+        _.each(analysis.slice(1), function (period, periodNumber) {
             _.each(period.stats, function (data) {
-                // TODO: calculate suggestions in here!
+                var index, hitsAreRising, numberIsHot, stillRising;
+
+                // current number was not hot in the latest period
+                if (!suggestions.map.hasOwnProperty(data.number)) {
+                    return;
+                }
+                index = suggestions.map[data.number];
+
+                // update risings
+                stillRising = suggestions.result[index].stillRising;
+                numberIsHot = (data.hits > averageHit);
+                hitsAreRising = (data.hits <= suggestions.result[index].previousHits);
+                if (stillRising && numberIsHot && hitsAreRising) {
+                    suggestions.result[index].risings++;
+                }
+
+                // update the still rising flag - stops calculations if the number has landed the average hit rate
+                if (stillRising && !numberIsHot) {
+                    suggestions.result[index].stillRising = false;
+                }
+
+                // move previous hits to next period
+                suggestions.result[index].previousHits = data.hits;
             });
         });
+
+        // sort results by risings
+        this.result.sort(function (a, b) {
+            return a.risings - b.risings;
+        });
+        this.result.reverse();
     }
 };
