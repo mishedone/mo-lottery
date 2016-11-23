cfc62160f1f654fc7bb78c979766ac1416ba2b64
mishedone <michamilev@gmail.com>
2016-11-23 22:50:23 +0200

    Initialize period factory for elapse time trend analysis.

---
+++ src/analysers/elapse-time-trend/factory/period.js
@@ -0,0 +1,6 @@
+function ElapseTimeTrendPeriodFactory() {}
+
+HotColdTrendPeriodFactory.prototype = {
+    constructor: HotColdTrendPeriodFactory,
+    
+    get: function (numbers, draws, periodCount) {
