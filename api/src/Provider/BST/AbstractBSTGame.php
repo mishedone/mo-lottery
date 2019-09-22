c43784408f01fa28f8f71e29e778306fb2dad0ff
mishedon <michamilev@gmail.com>
2019-09-22 03:08:38 +0300

    update draw loading in abstract bst game to reflect the new bst website structure

--- api/src/Provider/BST/AbstractBSTGame.php
+++ api/src/Provider/BST/AbstractBSTGame.php
@@ -37,25 +37,28 @@ abstract class AbstractBSTGame extends AbstractGame
     public function getDraws($year)
     {
         $this->validateYear($year);
-        
+
         // load managers
         $drawManager = ManagerRepository::get()->getDrawManager(
             $this->getId(), $year
         );
         $lastParseManager = ManagerRepository::get()->getLastParseManager();
-        
+
         // handle current year vs archive year
-        if (in_array($year, [2016, 2017])) {
+        $currentYear = date('Y');
+        if ($year == $currentYear) {
             if (!$lastParseManager->has($this->getId())) {
                 $drawManager->set($this->currentYearParser->parse($year));
                 $lastParseManager->set($this->getId(), new \DateTime());
             }
+        } elseif ($year > 2017) {
+            // TODO: handle new format archive year
         } else {
             if (!$drawManager->has()) {
                 $drawManager->set($this->archiveYearParser->parse($year));
             }
         }
-        
+
         return $drawManager->get();
     }
 
